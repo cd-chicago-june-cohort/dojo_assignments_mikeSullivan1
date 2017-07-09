7d6634dc32f170f05b6d9f40ebc34f1a9394ebd7
MikeSully <sullyut@gmail.com>
2017-07-08 19:34:40 -0500

    Adding Counter

---
+++ flask_fundamentals/counter/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template,redirect,request,session
+
+app = Flask(__name__)
+app.secret_key = 'changes234'
+
+@app.route('/')
+def index():
+    if 'y' not in session:
+        session['y'] = 0
+    session['y']+= 1  
+    return render_template('index.html')
+
+@app.route('/redir')
+def redir():
+    
+    return redirect('/')
+
+app.run(debug=True)
